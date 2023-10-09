94757cc2821fb15411a3ed40fcb31f5e21dbcc48
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-10 00:14:36 +0530

    Create Footer.jsx

---
+++ apple/src/Hm-backgrd-img/Footer.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import './Footer.css'
+import { Paragraph, Paragraphbold } from './Paragraph'
+const Footer = () => {
+  return (
+    <div id='Footer'>
+      <div>
+        <Paragraph>*Instant savings, otherwise referred to as instant cashback on the Apple Store Online, is available with the purchase of an eligible product for qualifying HDFC Bank Credit Cards and EasyEMI Credit Cards only. Minimum transaction value of ₹10001.00 applies. Click here to see instant savings amounts and eligible devices. Instant savings is available for up to two orders per rolling 90-day period with an eligible card. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Total transaction value is calculated after any trade-in credit or eligible discount applied. Any subsequent order adjustment(s) or cancellation(s) may result in instant savings being recalculated, and any refund may be adjusted to account for instant savings clawback; this may result in no refund being made to you. Offer may be revised or withdrawn at any time without any prior notice. Terms and conditions apply. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Multiple separate orders cannot be combined for instant savings.</Paragraph>
+        <Paragraph>A subscription is required for Apple TV+.</Paragraph>
+
+        <div>
+          <div>
+            <Paragraphbold>Shop and Learn</Paragraphbold>
+            <Paragraph>Store</Paragraph>
+            <Paragraph>Mac</Paragraph>
+            <Paragraph>iPad</Paragraph>
+            <Paragraph>iPhone</Paragraph>
+            <Paragraph>Watch</Paragraph>
+            <Paragraph>AirPods</Paragraph>
+            <Paragraph>TV & Home</Paragraph>
+            <Paragraph>AirTag</Paragraph>
+            <Paragraph>Accessories</Paragraph>
+            <Paragraph>Gift Cards</Paragraph>
+
+            <Paragraphbold>Apple Wallet</Paragraphbold>
+            <Paragraph>Wallet</Paragraph>
+          </div>
+          <div>
+            <Paragraphbold>Account</Paragraphbold>
+            <Paragraph>Manage Your Apple ID</Paragraph>
+            <Paragraph>Apple Store Account</Paragraph>
+            <Paragraph>iCloud.com</Paragraph>
+            <Paragraphbold>Entertainment</Paragraphbold>
+            <Paragraph>Apple One</Paragraph>
+            <Paragraph>Apple TV+</Paragraph>
+            <Paragraph>Apple Music</Paragraph>
+            <Paragraph>Apple Arcade</Paragraph>
+            <Paragraph>Apple Podcasts</Paragraph>
+            <Paragraph>Apple Books</Paragraph>
+            <Paragraph>App Store</Paragraph>
+          </div>
+          <div>
+            <Paragraphbold>Apple Store</Paragraphbold>
+            <Paragraph>Find a Store</Paragraph>
+            <Paragraph>Genius Bar</Paragraph>
+            <Paragraph>Today at Apple</Paragraph>
+            <Paragraph>Apple Camp</Paragraph>
+            <Paragraph>Apple Trade In</Paragraph>
+            <Paragraph>Ways to Buy</Paragraph>
+            <Paragraph>Recycling Programme</Paragraph>
+            <Paragraph>Order Status</Paragraph>
+            <Paragraph>Shopping Help</Paragraph>
+          </div>
+          <div>
+            <Paragraphbold>For Business</Paragraphbold>
+            <Paragraph>Apple and Business</Paragraph>
+            <Paragraph>Shop for Business</Paragraph>
+            <Paragraphbold>For Education</Paragraphbold>
+            <Paragraph>Apple and Education</Paragraph>
+            <Paragraph>Shop for Education</Paragraph>
+            <Paragraph>Shop for University</Paragraph>
+            <Paragraphbold>For Healthcare</Paragraphbold>
+            <Paragraph>Apple in Healthcare</Paragraph>
+            <Paragraph>Health on Apple Watch</Paragraph>
+          </div>
+          <div>
+            <Paragraphbold>Apple Values</Paragraphbold>
+            <Paragraph>Accessibility</Paragraph>
+            <Paragraph>Education</Paragraph>
+            <Paragraph>Environment</Paragraph>
+            <Paragraph>Privacy</Paragraph>
+            <Paragraph>Supplier Responsibility</Paragraph>
+            <Paragraphbold>About Apple</Paragraphbold>
+            <Paragraph>Newsroom</Paragraph>
+            <Paragraph>Apple Leadership</Paragraph>
+            <Paragraph>Career Opportunities</Paragraph>
+            <Paragraph>Investors</Paragraph>
+            <Paragraph>Ethics & Compliance</Paragraph>
+            <Paragraph>Events</Paragraph>
+            <Paragraph>Contact Apple</Paragraph>
+          </div>
+        </div>
+        <Paragraph>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</Paragraph>
+        <div>
+        <Paragraph>Copyright © 2023 Apple Inc. All rights reserved.</Paragraph>
+          <div>
+            <Paragraph>Privacy Policy | Terms of Use | Sales Policy | Legal | Site Map</Paragraph>
+          </div>
+          <Paragraph>India</Paragraph>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
