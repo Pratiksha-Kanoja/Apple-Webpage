0161f87b4486e26d2ef43ec2bec7729dbeaff2e1
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-11 03:10:53 +0530

    Update App.jsx

--- apple/src/App.jsx
+++ apple/src/App.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import Store from "./Mainpage/Store";
 import Homepage from "./Mainpage/Homepage";
-
+import Slidebar from './Mainpage/Slidebar';
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Homepage/>} />
+        <Route exact path='/store' element={<Store/>} />
+        <Route exact path='/slidebar' element={<Slidebar/>} />
       </Routes>
     </div>
   );
