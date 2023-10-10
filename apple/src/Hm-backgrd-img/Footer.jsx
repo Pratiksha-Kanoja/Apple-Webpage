b199e54d7bda530089588fdd310063b5b60da72c
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-11 03:11:26 +0530

    Update Footer.jsx

--- apple/src/Hm-backgrd-img/Footer.jsx
+++ apple/src/Hm-backgrd-img/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Footer.css'
 import { Paragraph, Paragraphbold } from './Paragraph'
-const Footer = () => {
+const Footer = ({children}) => {
   return (
     <div id='Footer'>
       <div>
-        <Paragraph>*Instant savings, otherwise referred to as instant cashback on the Apple Store Online, is available with the purchase of an eligible product for qualifying HDFC Bank Credit Cards and EasyEMI Credit Cards only. Minimum transaction value of ₹10001.00 applies. Click here to see instant savings amounts and eligible devices. Instant savings is available for up to two orders per rolling 90-day period with an eligible card. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Total transaction value is calculated after any trade-in credit or eligible discount applied. Any subsequent order adjustment(s) or cancellation(s) may result in instant savings being recalculated, and any refund may be adjusted to account for instant savings clawback; this may result in no refund being made to you. Offer may be revised or withdrawn at any time without any prior notice. Terms and conditions apply. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Multiple separate orders cannot be combined for instant savings.</Paragraph>
-        <Paragraph>A subscription is required for Apple TV+.</Paragraph>
-
+        
+        <Paragraph>{children}</Paragraph>
         <div>
           <div>
             <Paragraphbold>Shop and Learn</Paragraphbold>
