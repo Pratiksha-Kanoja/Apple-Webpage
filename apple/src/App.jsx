6316660bb0f8f7493c3495c1c65732a6760a8475
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-18 01:23:20 +0530

    Update App.jsx

--- apple/src/App.jsx
+++ apple/src/App.jsx
@@ -4,15 +4,17 @@ import Homepage from "./Mainpage/Homepage";
 import Slidebar from './Mainpage/Slidebar';
 import Registration from "./Mainpage/Registration";
 import Signin from "./Mainpage/Signin";
+import Apple15pro from "./Mainpage/Apple15pro";
 function App() {
   return (
     <div>
       <Routes>
-        {/* <Route path='/' element={<Homepage/>} />
+        <Route path='/' element={<Homepage/>} />
         <Route exact path='/store' element={<Store/>} />
-        <Route exact path='/slidebar' element={<Slidebar/>} /> */}
-        {/* <Route exact path="/" element={<Registration/>} /> */}
-        <Route exact path="/" element={<Signin/>} />
+        <Route exact path='/slidebar' element={<Slidebar/>} />
+        {/* <Route exact path="/register" element={<Registration/>} />
+        <Route exact path="/" element={<Signin/>} /> */}
+        {/* <Route exact path="/iPhone15Pro" element={<Apple15pro/>} /> */}
       </Routes>
     </div>
   );
