17044f38001fe5457b107a581a9a4e28208dd46c
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2024-04-27 23:31:27 +0530

    Update AxiosConfig.js

--- apple/src/Helpers/AxiosConfig.js
+++ apple/src/Helpers/AxiosConfig.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 // var BackendUrl = "http://localhost:8000/api/v1"
 
-if(process.env.MODE === "PRODUCTION"){
+//if(process.env.MODE === "PRODUCTION"){
    var BackendUrl = "https://apple-clone-backend.onrender.com/api/v1"
-}
+//}
 
 const api = axios.create({baseURL: BackendUrl})
 
